533c28411269fe3680a1c278c8ff536f318a524b
melodyWxy <44901373+melodyWxy@users.noreply.github.com>
2018-11-11 08:17:50 +0800

    'goodConfig'

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ActionSheet from './components/ActionSheet/index.jsx';
+import Header from './components/Header/index.jsx';
+
+
+class App extends Component{
+    render(){
+        return (
+            <div >
+                <ActionSheet />
+                <Header />
+            </div>
+        )
+    }
+}
+const div = document.createElement('div');
+document.body.appendChild(div);
+ReactDOM.render(<App />,div);
+
+
+
